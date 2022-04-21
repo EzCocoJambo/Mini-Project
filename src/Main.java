216f94ebe1f4cfaf4550b2391ee72c2c337739c8
Timela Vogli <timela.vogli@cit.edu.al>
2022-04-21 22:13:28 +0200

    decoding also works

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,14 @@ public class Main {
         char[] letters = { ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
                 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
 
-        String[] morseLetters = { "   ", ". ___", "___ . . .", "___ . ___ .", "___ . .", ".", ". . ___ .", "___ ___ .",
-                ". . . .", ". .", ". ___ ___ ___", "___ . ___", ". ___ . .", "___ ___", "___ .", "___ ___ ___",
-                ". ___ ___ .", "___ ___ . ___", ". ___ .", ". . .", "_", ". . ___", ". . . ___", ". ___ ___",
-                "___ . . ___", "___ . ___ ___", "___ ___ . .", ". ___ ___ ___ ___", ". . ___ ___ ___", ". . . ___ ___",
-                ". . . . ___", ". . . . .", "___ . . . .", "___ ___ . . .", "___ ___ ___ . .", "___ ___ ___ ___ .",
-                "___ ___ ___ ___ ___" };
+        String[] morseLetters = { " ", ".-", "-...", "-.-.", "-..", ".", "..-..", "--.",
+                "....", "..", ".---", "-.-", ".-..", "--", "-.", "---",
+                ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--",
+                "-..-", "-.--", "--..", ".----", "..---", "...--",
+                "....-", ".....", "-....", "--...", "---..", "----.",
+                "-----" };
 
-        String textToChange = "";
+        String textToChange;
         String newText = "";
         System.out.println("Enter text you want to change to Morse code");
         textToChange = input.nextLine();
@@ -25,7 +25,8 @@ public class Main {
             for (short j = 0; j < letters.length; j++) {
                 if (textToChange.charAt(i) == letters[j]) {
                     newText += morseLetters[j];
-                    newText += "   ";
+                    if (letters[j] != ' ')
+                        newText += " ";
                     break;
                 }
             }
@@ -34,12 +35,12 @@ public class Main {
         System.out.println("Text in Morse Code: ");
         System.out.println(newText);
 
-        String codeToChange = "";
+        String codeToChange;
         String decodedText = "";
         System.out.println("Enter Morse code you want to change to Text");
         codeToChange = input.nextLine();
 
-        String[] codes = codeToChange.split(" {3}");
+        String[] codes = codeToChange.split(" ");
         for (int i = 0; i < codes.length; i++) {
             for (int j = 0; j < morseLetters.length; j++) {
                 if (codes[i].equals(morseLetters[j])) {
@@ -47,9 +48,9 @@ public class Main {
                     break;
                 }
             }
-//            if (i != codes.length - 1) {
-//                decodedText += " ";
-//            }
+            if (codes[i].equals("")) {
+                decodedText += " ";
+            }
         }
         System.out.println("Morse Code in Text: ");
         System.out.println(decodedText);
